89b51ac04e07386cf3519b52a14f9fdca8a898a9
Ben <contact@motelcoach.com.au>
2026-03-27 15:06:56 +1000

    urlfinal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: 'https://motelcoach.com.au',
   trailingSlash: 'always',
   output: 'static',
-  
 });
